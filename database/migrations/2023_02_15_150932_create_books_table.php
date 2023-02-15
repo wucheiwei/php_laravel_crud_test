98916d200f399c024aff4c3edf7826c7792d37aa
zack <twd1000015706@gmail.com>
2023-02-15 23:30:52 +0800

    modle

---
+++ database/migrations/2023_02_15_150932_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string(column:'title');
+            $table->text(column:'description');
+            $table->decimal(column:'price');
+            $table->boolean(column:'available')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
